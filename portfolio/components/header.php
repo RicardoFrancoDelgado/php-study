47c59b09341a6a8421e4a3383a1288bdb1451e46
RicardoFrancoDelgado <rf041555@gmail.com>
2026-03-30 12:10:49 -0300

    feat: tornando o header dinâmico

--- portfolio/components/header.php
+++ portfolio/components/header.php
@@ -1,38 +1,31 @@
+<?php
+$logoPresentation = "🦈 Ricardo Franco ...";
+
+$itens = [
+  ['href' => '#projetos', 'texto' => 'Projetos'],
+  ['href' => 'https://github.com/RicardoFrancoDelgado', 'texto' => 'Github'],
+  ['href' => 'https://linkedin.com/in/ricardofrancodelgado-dev', 'texto' => 'LinkedIn'],
+  ['href' => 'https://youtube.com/@ricardiinn', 'texto' => 'Youtube'],
+];
+?>
+
 <header
   class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
   <!-- Logo -->
-  <div class="font-bold text-xl text-cyan-600">🦈 Ricardo Franco ...</div>
+  <div class="font-bold text-xl text-cyan-600">
+    <?= $logoPresentation ?>
+  </div>
 
   <!-- Links -->
   <div class="">
     <ul class="flex items-center gap-3 font-medium">
-      <li>
-        <a href="#projetos" class="hover:underline"> Projetos </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/RicardoFrancoDelgado"
-          target="_blank"
-          class="hover:underline">
-          Github
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://linkedin.com/in/ricardofrancodelgado-dev"
-          target="_blank"
-          class="hover:underline">
-          Linkedin
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://youtube.com/@ricardiinn"
-          target="_blank"
-          class="hover:underline">
-          Youtube
-        </a>
-      </li>
+      <?php foreach ($itens as $item): ?>
+        <li>
+          <a href=<?= $item['href'] ?> class="hover:underline " target="_blank">
+            <?= $item['texto'] ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
     </ul>
   </div>
 </header>
